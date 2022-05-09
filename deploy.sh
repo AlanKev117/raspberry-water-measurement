a3706467201df05d97c19b09629336216b209924
AlanKev117 <fuentes.alan.98@gmail.com>
2022-05-08 20:29:04 -0500

    Merged deployment prodedure in single script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+set -e
+
+CWD=$(pwd)
+
 # Install requirements
 pip3.8 install -r requirements.txt
 
 # Copy service file to right path
-SYSTEMDIR="$HOME/.config/systemd/user"
+SYSTEMDIR="/etc/systemd/system/"
 [ -d $SYSTEMDIR ] || mkdir -p $SYSTEMDIR
 cp water_level.service $SYSTEMDIR
 
@@ -13,6 +17,16 @@ SERVICE_FILE="$SYSTEMDIR/water_level.service"
 [ -f "$SERVICE_FILE" ] || ( echo "Missing service file in right location" && exit 1)
 
 # Enable service to run on boot
-source water.env
-systemctl enable --user --now water_level.service
-sleep 1
+systemctl enable water_level.service
+
+# Set up env vars to be loaded on boot
+WATER_ENV_FILE=$CWD/water.env
+
+if grep -Fxq "source ${WATER_ENV_FILE}" "/etc/profile" 
+then
+    exit 0
+else
+    echo "" >> /etc/profile
+    echo "source ${WATER_ENV_FILE}" >> /etc/profile
+    echo "" >> /etc/profile
+fi
