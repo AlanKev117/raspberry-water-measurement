a40a285bd3aba8def0565c7a552582334b86b833
AlanKev117 <fuentes.alan.98@gmail.com>
2022-05-08 20:04:02 -0500

    Env vars sourced from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 # Install requirements
 pip3.8 install -r requirements.txt
 
@@ -12,8 +10,9 @@ cp water_level.service $SYSTEMDIR
 
 # Assert service got copied to right path
 SERVICE_FILE="$SYSTEMDIR/water_level.service"
-[ -f "$SERVICE_FILE" ] || ( echo "Missing service file in right location, make sure to run setup.sh first" && exit 1)
+[ -f "$SERVICE_FILE" ] || ( echo "Missing service file in right location" && exit 1)
 
 # Enable service to run on boot
+source water.env
 systemctl enable --user --now water_level.service
-
+sleep 1
