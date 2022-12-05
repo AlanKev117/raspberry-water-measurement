c826c14531a7ba722ae87b1dc134372be1f4055d
AlanKev117 <fuentes.alan.98@gmail.com>
2022-12-04 21:41:49 -0600

    Added publisher service

--- deploy.sh
+++ deploy.sh
@@ -12,17 +12,23 @@ pip3.8 install -r requirements.txt
 [ -d "$INSTALLATION_DIR" ] && rm -rf $INSTALLATION_DIR
 mkdir $INSTALLATION_DIR
 cp -r $CWD/src/* $INSTALLATION_DIR
+cp -r $CWD/iot/* $INSTALLATION_DIR
 cp -r $CWD/water.env $INSTALLATION_DIR
+cp -r $CWD/publisher.env $INSTALLATION_DIR
 
-# Copy service file to the right location for systemd.
+# Copy service files to the right location for systemd.
 SYSTEMDIR="/etc/systemd/system/"
 [ -d $SYSTEMDIR ] || mkdir -p $SYSTEMDIR
-cp water_level.service $SYSTEMDIR
+cp water_level.service level_publisher.service $SYSTEMDIR
 
-# Assert service got copied to the right location.
+# Assert services got copied to their right location.
 SERVICE_FILE="$SYSTEMDIR/water_level.service"
-[ -f "$SERVICE_FILE" ] || ( echo "Missing service file in right location" && exit 1)
+[ -f "$SERVICE_FILE" ] || ( echo "Missing water level service file in right location" && exit 1)
+SERVICE_FILE="$SYSTEMDIR/level_publisher.service"
+[ -f "$SERVICE_FILE" ] || ( echo "Missing level publisher service file in right location" && exit 1)
 
-# Enable service to run on boot.
+# Enable services to run on boot.
 systemctl enable water_level.service
 systemctl restart water_level.service
+systemctl enable level_publisher.service
+systemctl restart level_publisher.service
---
+++ src/publish_level.py
@@ -0,0 +1,48 @@
+import os
+import json
+import time
+
+import requests
+
+http_endpoint = os.getenv("HTTP_ENDPOINT", None)
+iot_endpoint = os.getenv("IOT_ENDPOINT", None)
+topic = os.getenv("TOPIC", None)
+cert = os.getenv("CERT", None)
+key = os.getenv("KEY", None)
+
+vars = [http_endpoint, iot_endpoint, topic, cert, key]
+
+assert None not in vars, "ERROR: There is at least one variable missing for the level publisher!"
+
+# create and format values for HTTPS request
+publish_url = 'https://' + iot_endpoint + ':8443/topics/' + topic + '?qos=1'
+
+month_millis = 1000 * 60 * 60 * 24 * 30
+
+while True:
+
+  try:
+    measure_response = requests.get(http_endpoint)
+    assert measure_response.status_code == 200
+    level = measure_response.json()["level"]
+
+    data = {
+      "level": level,
+      "expiration_time": int(time.time() * 1000) + month_millis
+    }
+    publish_msg = json.dumps(data)
+
+    # Publish measurement
+    publish = requests.request('POST',
+      publish_url,
+      data=publish_msg,
+      cert=[cert, key])
+
+    print("Response status: ", str(publish.status_code))
+
+  except:
+    print("Error reading sensor or sending message.")
+    continue
+
+  # Wait 10 seconds until next iteration.
+  time.sleep(10)
